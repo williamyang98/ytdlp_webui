8705065451ed45deb67e54ab1e61d1ae0fb2996d
William Yang <williamyang2017@gmail.com>
2026-05-19 01:33:12 +1000

    Handle responses with bad http status codes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -27,8 +27,14 @@ const BASE_URL =
   `${window.location.protocol}//${window.location.host}`;
 const API_URL = "api/v1";
 
+async function handle_bad_response(response: Response) {
+  const body = await response.text();
+  throw new Error(`Bad response status ${response.status} (${response.statusText}): ${body}`);
+}
+
 export async function request_transcode(key: TranscodeKey): Promise<RequestTranscodeResponse> {
   const response = await fetch(`${BASE_URL}/${API_URL}/request_transcode/${key.video_id}/${key.audio_ext}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = RequestTranscodeResponseSchema.parse(json);
   return state;
@@ -36,6 +42,7 @@ export async function request_transcode(key: TranscodeKey): Promise<RequestTrans
 
 export async function delete_download(key: DownloadKey): Promise<DeleteResponse> {
   const response = await fetch(`${BASE_URL}/${API_URL}/delete_download/${key}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = DeleteResponseSchema.parse(json);
   return state;
@@ -43,6 +50,7 @@ export async function delete_download(key: DownloadKey): Promise<DeleteResponse>
 
 export async function delete_transcode(key: TranscodeKey): Promise<DeleteResponse> {
   const response = await fetch(`${BASE_URL}/${API_URL}/delete_transcode/${key.video_id}/${key.audio_ext}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = DeleteResponseSchema.parse(json);
   return state;
@@ -50,6 +58,7 @@ export async function delete_transcode(key: TranscodeKey): Promise<DeleteRespons
 
 export async function get_downloads(): Promise<YtdlpRow[]> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_downloads`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = YtdlpRowSchema.array().parse(json);
   return state;
@@ -57,6 +66,7 @@ export async function get_downloads(): Promise<YtdlpRow[]> {
 
 export async function get_transcodes(): Promise<FfmpegRow[]> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_transcodes`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = FfmpegRowSchema.array().parse(json);
   return state;
@@ -64,6 +74,7 @@ export async function get_transcodes(): Promise<FfmpegRow[]> {
 
 export async function get_download(key: DownloadKey): Promise<YtdlpRow> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_download/${key}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = YtdlpRowSchema.parse(json);
   return state;
@@ -71,6 +82,7 @@ export async function get_download(key: DownloadKey): Promise<YtdlpRow> {
 
 export async function get_transcode(key: TranscodeKey): Promise<FfmpegRow> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_transcode/${key.video_id}/${key.audio_ext}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = FfmpegRowSchema.parse(json);
   return state;
@@ -78,6 +90,7 @@ export async function get_transcode(key: TranscodeKey): Promise<FfmpegRow> {
 
 export async function get_download_state(key: DownloadKey): Promise<DownloadState> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_download_state/${key}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = DownloadStateSchema.parse(json);
   return state;
@@ -85,6 +98,7 @@ export async function get_download_state(key: DownloadKey): Promise<DownloadStat
 
 export async function get_transcode_state(key: TranscodeKey): Promise<TranscodeState> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_transcode_state/${key.video_id}/${key.audio_ext}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = TranscodeStateSchema.parse(json);
   return state;
@@ -92,6 +106,7 @@ export async function get_transcode_state(key: TranscodeKey): Promise<TranscodeS
 
 export async function get_metadata(key: DownloadKey): Promise<YoutubeMetadata> {
   const response = await fetch(`${BASE_URL}/${API_URL}/get_metadata/${key}`);
+  if (!response.ok) await handle_bad_response(response);
   const json = await response.json();
   const state = YoutubeMetadataSchema.parse(json);
   return state;
