ab182feafabec0ef0343e3a218b46408b14f240f
William Yang <williamyang2017@gmail.com>
2026-05-20 01:00:19 +1000

    Fixed github actions by importing environment variables and installing node via nvm

--- scripts/download_ubuntu.sh
+++ scripts/download_ubuntu.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sudo apt-get --yes install ffmpeg python3 npm
+sudo apt-get --yes install ffmpeg python3 curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+. $HOME/.nvm/nvm.sh
+nvm install --lts
 pip install yt-dlp
